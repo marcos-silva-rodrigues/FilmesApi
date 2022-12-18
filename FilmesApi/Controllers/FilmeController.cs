2c8eda194d4ade7846059e045210f732555866bf
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-12-17 21:07:08 -0300

    Primeiro controller e algumas anotações de validações

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,23 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace FilmesApi.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme);
+        }
+    }
+}
