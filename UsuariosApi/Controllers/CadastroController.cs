e8996752846e2bd8fa216680cb39956036e4f4c8
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2023-01-23 21:52:49 -0300

    feat: api para login e cadastro esqueleto

---
+++ UsuariosApi/Controllers/CadastroController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UsuariosApi.Controllers
+{
+
+	[ApiController]
+	[Route("[controller]")]
+	public class CadastroController : ControllerBase
+	{
+
+		[HttpPost]
+		public IActionResult CadastroUsuario(CreateUsuarioDto dto)
+		{
+			return Ok();
+		}
+	}
+}
