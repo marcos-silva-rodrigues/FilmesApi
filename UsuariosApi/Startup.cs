84f431e73f6cb985759e591d77e221dfa54c933a
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2023-01-23 21:53:52 -0300

    feat: config do automapper

--- UsuariosApi/Startup.cs
+++ UsuariosApi/Startup.cs
@@ -28,10 +28,7 @@ namespace UsuariosApi
 		{
 
 			services.AddControllers();
-			services.AddSwaggerGen(c =>
-			{
-				c.SwaggerDoc("v1", new OpenApiInfo { Title = "UsuariosApi", Version = "v1" });
-			});
+			services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
